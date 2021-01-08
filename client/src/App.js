ee3fd447bacd10388305c248c29c88637182b7bd
Tsin Moua <tsinmoua@gmail.com>
2021-01-08 10:14:10 -0600

    Added nominated page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Nominated from "./pages/Nominated";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Navbar>
           <Route exact path="/" component={Home} />
+          <Route exact path="/nominated" component={Nominated} />
         </Navbar>
       </Switch>
     </BrowserRouter>
---
+++ client/src/pages/Nominated.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Nominated = (props) => {
+    return (
+        <div>
+            Nominated
+        </div>
+    )
+};
+
+export default Nominated;
