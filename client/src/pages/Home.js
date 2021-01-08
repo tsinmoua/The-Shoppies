d8b3aed6c1c2f33a906e14b395bc094687456f04
Tsin Moua <tsinmoua@gmail.com>
2021-01-07 20:24:22 -0600

    Added standard card formatting to movies search

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Button, Grid, makeStyles, TextField, Typography } from '@material-ui/core';
+import { Button, Card, CardActions, CardContent, CardMedia, Grid, makeStyles, TextField, Typography } from '@material-ui/core';
 
 import API from "../utils/API";
 
@@ -18,21 +18,24 @@ const Home = (props) => {
     const classes = useStyles();
 
     const [search, setSearch] = useState("")
-    const [results, setResults] = useState('');
+    const [results, setResults] = useState([]);
 
     function searchMovies(query) {
         API.search(query)
             .then(res => {
-                console.log(res);
-                // console.log(res.data.items);
-                // setResults(res.data.items)
+                // console.log(res);
+                setResults(res.data.Search)
             })
             .catch(err => console.log(err));
     };
 
+    console.log(results)
+    // console.log(results.length)
+    // console.log(results[0].Poster)
+
     function handleInputChange(event) {
         const { value } = event.target;
-        console.log(value);
+        // console.log(value);
         setSearch(value)
     };
 
@@ -40,12 +43,14 @@ const Home = (props) => {
         event.preventDefault();
         searchMovies(search)
         setSearch("");
-      };
+    };
+
+
 
     return (
         <React.Fragment>
-            <Grid container direction='column' justify='center' alignItems='center'>
 
+            <Grid container direction='column' justify='center' alignItems='center'>
                 <Grid item >
                     <Typography variant='h1' gutterBottom>
                         The Shoppies
@@ -56,21 +61,51 @@ const Home = (props) => {
                     <form onSubmit={handleFormSubmit}>
                         <TextField
                             id="outlined-basic"
-                            label="Search a movie by Title"
+                            label="Search for a Movie"
                             variant="outlined"
                             className={classes.searchInput}
                             onChange={handleInputChange}
                             value={search}
                             autoFocus
                         />
-                        <Button variant='contained' className={classes.button} onClick={handleFormSubmit}>
+                        <Button
+                            variant='contained'
+                            className={classes.button}
+                            onClick={handleFormSubmit}
+                            type='submit'
+                        >
                             Search
-                    </Button>
+                        </Button>
                     </form>
                 </Grid>
-
-
             </Grid>
+
+            {results.length === 0 ? null :
+                <Grid container justify='center' alignItems='center'>
+                    <Card>
+                        <CardMedia
+                            component='img'
+                            alt={results[0].Title}
+                            src={results[0].Poster}
+                            title={results[0].Title}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5">
+                                {results[0].Title}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary">
+                                Year Released: {results[0].Year}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button variant='contained' color='primary'>
+                                Nominate
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            }
+
         </React.Fragment>
     )
 };
