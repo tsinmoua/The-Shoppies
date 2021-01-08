3a5d0236461160ad532b1ddc55e00e9358e999ba
Tsin Moua <tsinmoua@gmail.com>
2021-01-07 19:07:36 -0600

    Added API file to client

---
+++ client/src/utils/API.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+
+const APIKEY = process.env.REACT_APP_OMDB_KEY;
+
+export default {
+  search: function (query) {
+    return axios.get("http://www.omdbapi.com/?apikey=" + APIKEY + "&t=" + query);
+  },
+
+  save: function (movieData) {
+    return axios.post("/api/movie", movieData);
+  },
+
+  get: function () {
+    return axios.get("/api/movie");
+  },
+
+  delete: function (id) {
+    return axios.delete("/api/movie/" + id);
+  },
+};
